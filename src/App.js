935b56adae6a6964de62820300b2dfd3167fecdc
Nouman Javed <noumanmalik960@gmail.com>
2020-11-18 16:57:35 +0500

    added one more test

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ function App() {
   return (
     <div className="container">
       <h2>Sup!</h2>
+      <h3>Salam alaikum</h3>
+      <img src='' alt='logo' />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -4,9 +4,12 @@ import App from './App';
 test('renders learn react link', () => {
   render(<App />);
   // i in regex is for ignoring case sensitivity
+  // const linkElement = screen.getByText(/sup/i);
+  // expect(linkElement).toBeInTheDocument();
+
   const linkElement = screen.getByText(/sup/i);
   expect(linkElement).toBeInTheDocument();
 
-  // const imageElement = screen.getByAltText('logo')
-  // expect(imageElement).toBeInTheDocument();
+  const imageElement = screen.getByAltText('logo')
+  expect(imageElement).toBeInTheDocument();
 });
